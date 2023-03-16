99f59abe43e4b84dc666ee639a0a7dfb33feefb7
Nikita <nikita.gon.dev@gmail.com>
2023-03-16 20:43:28 +0100

    updates

--- main.cpp
+++ main.cpp
@@ -43,22 +43,37 @@ int main()
 
 	// Rendering variables
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
+		0.5f,  0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
 	};
-
-	unsigned int VBO;
-
-
 
 	/* triangle stuff */
 
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+
+
+	unsigned int VBO;
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+
+	unsigned int indices[] = { 
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Vertex
 	const char* vertexShaderSource = "#version 330 core\n"
@@ -76,7 +91,7 @@ int main()
 
 	///////////////////////////
 
-	int  success;
+	int success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 
@@ -118,10 +133,15 @@ int main()
 		return 1;
 	}
 	
-	glUseProgram(shaderProgram);
+	// Linking shaders to vertexes, pasting them with glue
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -133,7 +153,16 @@ int main()
 		glClearColor(0.39f, 0.58f, 0.92f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		
+		// THE TRIANGLE!!!
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(0);
 
 		// Update stuff here:
 		glfwSwapBuffers(window);
